0304d1acd31309cf138021b474e4f55bb8d0b532
nasta1717 <anastasismihailenko@gmail.com>
2025-04-28 19:48:04 +0300

    dz

---
+++ test/дискриминант.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение a: ");
+            string sta = Console.ReadLine();
+            double a = Convert.ToInt32(sta);
+            if (a == 0)
+            {
+                Console.WriteLine("Значение а не может быть равно 0");
+            }
+            else
+            {
+                Console.WriteLine("Введите значение b: ");
+                string stb = Console.ReadLine();
+                double b = Convert.ToInt32(stb);
+                if (b == 0)
+                {
+                    Console.WriteLine("Значение b не может быть равно 0");
+                }
+                else
+                {
+                    Console.WriteLine("Введите значение c: ");
+                    string stc = Console.ReadLine();
+                    double c = Convert.ToInt32(stc);
+                    if (c == 0)
+                    {
+                        Console.WriteLine("Значение c не может быть равно 0");
+                    }
+                    else
+                    {
+                        double dis = 0;
+                        dis = ((Math.Pow(b, 2)) - (4 * a * c));
+
+                        Console.WriteLine(dis);
+
+                        if (dis > 0)
+                        {
+                            double x1 = ((-1 * b) - Math.Sqrt(dis)) / (2 * a);
+                            double x2 = ((-1 * b) + Math.Sqrt(dis)) / (2 * a);
+                            Console.WriteLine(x1);
+                            Console.WriteLine(x2);
+
+                        }
+                        else
+                        {
+                            if (dis == 0)
+                            {
+                                double x1 = -b / (2 * a);
+                                double x2 = -b / (2 * a);
+                                Console.WriteLine(x1);
+                                Console.WriteLine(x2);
+
+                            }
+                            else
+                            {
+                                if (dis < 0)
+                                {
+                                    Console.WriteLine("Нет ироцирнаотных корней, есть действительные корни");
+
+                                }
+                                else
+                                {
+
+                                    dis = -dis;
+                                    double x1 = Math.Sqrt(dis) / (2 * a);
+                                    double x2 = -Math.Sqrt(dis) / (2 * a);
+                                    Console.WriteLine(x1 + " * i ");
+                                    Console.WriteLine(x2 + " * i ");
+
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
---
+++ test/хорды.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ол
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+  
+                Console.WriteLine("Введите значение первого х:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите значение второго х:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите значение эпсилы (Если оно меньше 1, вводить рекомендуется через знак точки, например 0.1):");
+                int epsila = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Значение Х методом половинного деления = {PolovinoeDelenie(a, b)} \n");
+                Console.WriteLine($"Значение Х методом хорд = {MetodHord(a, b, epsila)}\n");
+                Console.ReadKey();
+            }
+            static public int PolovinoeDelenie(int a, int b)
+            {
+                int x = 0;
+                x = (a + b) / 2;
+                double epsila = 0.001;
+                while (b - a > epsila)
+                {
+                    x = (a + b) / 2;
+                    if (FuncziaA(a, b) * FuncziaX(x, a, b) > 0)
+                    {
+                        a = x;
+                    }
+                    else
+                    {
+                        b = x;
+                    }
+
+                }
+
+                return x;
+            }
+            static public int MetodHord(int a, int b, int epsila)
+            {
+                int x = 0;
+                x = a - ((FuncziaA(a, b) / FuncziaB(b, a) - FuncziaA(a, b)) * (b - a));
+
+                while (FuncziaX(x, a, b) < epsila)
+                {
+                    x = a - ((FuncziaA(a, b) / FuncziaB(b, a) - FuncziaA(a, b)) * (b - a));
+                    if (FuncziaA(a, b) * FuncziaX(x, a, b) > 0)
+                    {
+                        a = x;
+                    }
+                    else
+                    {
+                        b = x;
+                    }
+
+                }
+
+                return x;
+            }
+            static public int FuncziaA(int a, int b)
+            {
+                a = 0;
+                return a;
+            }
+            static public int FuncziaB(int b, int a)
+            {
+
+                return b;
+            }
+            static public int FuncziaX(int x, int a, int b)
+            {
+
+                return x;
+            }
+
+        }
+    }
+
+
+    
+
